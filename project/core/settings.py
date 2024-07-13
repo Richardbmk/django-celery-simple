e94652b59590b8da296edf5d63e5dc127a811ea3
Richard <rdobmk@gmail.com>
2024-07-13 16:44:17 +0200

    article completed

--- project/core/settings.py
+++ project/core/settings.py
@@ -28,6 +28,10 @@ DEBUG = int(os.environ.get("DEBUG", default=0))
 
 ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
 
+# CELERY CONFIGURATION
+CELERY_BROKER_URL = os.environ.get("CELERY_BROKER", "redis://redis:6379/0")
+CELERY_RESULT_BACKEND = os.environ.get("CELERY_BROKER", "redis://redis:6379/0")
+
 
 # Application definition
 
